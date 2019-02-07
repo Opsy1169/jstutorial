a84bda5e325d6ebadb36ead1570c2c859492be31
Opsy1169 <rodinilyaalexeevich@gmail.com>
2019-02-07 16:45:36 +0400

    jquery rework

--- script.js
+++ script.js
@@ -1,67 +1,87 @@
 
+$(document).ready(function(){
 
-var range = document.getElementById("range");
-range.addEventListener("input", changeHeight)
-var select = document.getElementById("select");
-select.addEventListener("change", changeSymbol);
+
+const range = $('#range').on('input', changeHeight);
+
+
+// range.addEventListener("input", changeHeight)
+var select = $('#select').on('change', changeSymbol);
+
+// select.addEventListener("change", changeSymbol);
 printPyramid(6);
 
 
 function printPyramid(height) {
-  let rows = document.getElementsByClassName("row");
+  let rows = $(".row");
+
   a = "asdasdasd";
   if(rows.length == 0) {
-    var maxvalue = range.max;
-    console.log(maxvalue);
-	   a = "";
-	    let pyramid = document.getElementById('pyramid');
-  for(let i = 1; i <= maxvalue; ++i) {
-      let s = '';
-  		for (let j = 0; j < maxvalue-i; ++j ) {
-  			s += '\u00A0';
-  		}
-      for (let j = 0; j < i + 1; ++j) {
-  			s += '#';
-      }
-  		let elem  = document.createElement("p");
-  		elem.textContent = s;
-      elem.className = 'row';
-  		pyramid.appendChild(elem);
+    var maxvalue = range.prop('max');
 
-    }
-    for (let i = maxvalue - 1; i >= range.defaultValue; i -= 1) {
-      rows[i].setAttribute('style', 'display: none');
-    }
+	  a = "";
+	  let pyramid = $('#pyramid');
+      for(let i = 1; i <= maxvalue; ++i) {
+        let s = '';
+  		  for (let j = 0; j < maxvalue-i; ++j ) {
+  			  s += '\u00A0';
+  		  }
+        for (let j = 0; j < i + 1; ++j) {
+  			  s += '#';
+        }
+  		  let elem  = $("<p>");
+  		  elem.text(s);
+        elem.addClass('row');
+  		  pyramid.append(elem);
+
+        }
+    $.each(rows, function(row){
+      $(this).toggleClass('hidden');
+    })
 }
-const rowsarr = Array.prototype.filter.call(rows, (el) => { return !(el.style.display === 'none' || el.style.display === 'hidden')})
+// const rowsarr = Array.prototype.filter.call(rows, (el) => { return !(el.style.display === 'none' || el.style.display === 'hidden')})
+const rowsarr = rows.filter(':visible');
   if (rowsarr.length < height) {
-    for (let i = rowsarr.length - 1; i < height; ++i) {
-      rows[i].setAttribute('style', 'display: block');
-  }
+    for(let i = rowsarr.length; i < height; ++i){
+      rows.eq(i).removeClass('hidden').addClass('visible');
+    }
+  //   for (let i = rowsarr.length - 1; i < height; ++i) {
+  //     rows[i].setAttribute('style', 'display: block');
+  // }
 }
 else{
   for(let i = rowsarr.length-1; i >= height; --i) {
-    rows[i].setAttribute('style', 'display: none');
+    rows.eq(i).removeClass('visible').addClass('hidden');
   }
 }
-  document.getElementsByClassName('rangelabel')[0].textContent = height;
+ $('.rangelabel').text(height);
 }
 
 
 function changeHeight() {
-  const rangevalue = document.getElementById('range').value;
+  let rangevalue = $('#range').val();
   printPyramid(rangevalue);
 }
 
 function changeSymbol() {
-  let select = document.getElementById("select");
-  let symbol = select.options[select.selectedIndex].value;
-  let rows = document.getElementsByClassName("row");
-  for (let i = 0; i < rows.length; ++i){
-    var qwe = rows[i].textContent;
+  let select = $("#select option:selected").val();
+  console.log(select);
+  
+  let symbol = select;
+  let rows = $(".row");
+  $.each(rows, function(row){
+    let qwe = $(this).text();
     qwe = qwe.replace(/#/g, symbol);
     qwe = qwe.replace(/\$/g,  symbol);
     qwe = qwe.replace(/%/g,  symbol);
-    rows[i].textContent = qwe;
-  }
+    $(this).text(qwe);
+  })
+  // for (let i = 0; i < rows.length; ++i){
+  //   var qwe = rows[i].textContent;
+  //   qwe = qwe.replace(/#/g, symbol);
+  //   qwe = qwe.replace(/\$/g,  symbol);
+  //   qwe = qwe.replace(/%/g,  symbol);
+  //   rows[i].textContent = qwe;
+  // }
 }
+})
